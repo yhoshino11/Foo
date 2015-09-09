041ba5445feaea19715ac5d069893c00159a02b8
Yu Hoshino <yuhoshino@dev.local>
2015-09-09 21:55:46 +0900

    All ContentsController Test Passed

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,10 +1,41 @@
 class ContentsController < ApplicationController
   def index
+    @contents = User.find(params[:user_id]).contents.all
+  end
+
+  def show
+    @content = User.find(params[:user_id]).contents.find(params[:id])
   end
 
   def new
+    @content = current_user.contents.new
   end
 
   def edit
+    @content = current_user.contents.find(params[:id])
+  end
+
+  def create
+    @content = current_user.contents.new(content_params)
+    if @content.save
+      redirect_to user_contents_path(user_id: current_user)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @content = current_user.contents.find(params[:id])
+    if @content.update(content_params)
+      redirect_to user_contents_path(user_id: current_user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def content_params
+    params.require(:content).permit(:title, :text)
   end
 end
--- spec/controllers/contents_controller_spec.rb
+++ spec/controllers/contents_controller_spec.rb
@@ -4,12 +4,31 @@ RSpec.describe ContentsController, type: :controller do
   describe 'GET #index' do
     before do
       @user = create(:user)
+      @content = @user.contents.create(build(:content).attributes)
+      @contents = @user.contents.all
       sign_in(@user)
     end
 
     it 'returns http success' do
       get :index, user_id: @user
       expect(response).to have_http_status(:success)
+      expect(assigns(:contents)).to eq(@contents)
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET #show' do
+    before do
+      @user = create(:user)
+      @content = @user.contents.create(build(:content).attributes)
+      sign_in(@user)
+    end
+
+    it 'returns http success' do
+      get :show, user_id: @user, id: @content
+      expect(response).to have_http_status(:success)
+      expect(assigns(:content)).to eq(@content)
+      expect(response).to render_template(:show)
     end
   end
 
@@ -22,6 +41,22 @@ RSpec.describe ContentsController, type: :controller do
     it 'returns http success' do
       get :new, user_id: @user
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:new)
+      expect(assigns(:content)).to be_a_new(Content)
+    end
+  end
+
+  describe 'POST #create' do
+    before do
+      @user = create(:user)
+      @content = @user.contents.build(build(:content).attributes)
+      sign_in(@user)
+    end
+
+    it 'creates new content' do
+      expect do
+        post :create, user_id: @user, content: @content.attributes
+      end.to change(Content, :count).by(1)
     end
   end
 
@@ -35,6 +70,26 @@ RSpec.describe ContentsController, type: :controller do
     it 'returns http success' do
       get :edit, user_id: @user, id: @content
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:edit)
+      expect(assigns(:content)).to eq(@content)
+    end
+  end
+
+  describe 'PATCH #update' do
+    before do
+      @user = create(:user)
+      @content = @user.contents.create(build(:content).attributes)
+      sign_in(@user)
+    end
+
+    it 'updates content' do
+      put :update, user_id: @user,
+                   id: @content,
+                   content: attributes_for(:content, text: 'UPDATED')
+      expect(assigns(:content)).to eq(@content)
+      @content.reload
+      expect(@content.text).to eq('UPDATED')
+      expect(response).to redirect_to(user_contents_path(user_id: @user))
     end
   end
 end
